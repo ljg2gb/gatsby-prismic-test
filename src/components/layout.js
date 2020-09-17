4115f693817c5943978f1ba1e257aadc53ef076a
Lydia Gregory <ljg2gb@virginia.edu>
2020-09-17 16:26:29 -0400

    aqueries navigation data from Prrismic to Layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql, Link } from "gatsby"
 import "./layout.css"
 import styled from 'styled-components';
 
@@ -9,11 +9,46 @@ const Main = styled.main`
   margin: 0 auto
 `
 
-const Layout = ({ children }) => {
-  
+const navigationQuery = graphql`
+{
+  prismic {
+    allNavigations {
+      edges {
+        node {
+          navigation_links {
+            link {
+              ... on PRISMIC_Page {
+                _meta {
+                  uid
+                }
+              }
+            }
+            label
+          }
+        }
+      }
+    }
+  }
+}`
 
+const NavLink = styled.div``;
+
+const Layout = ({ children }) => {
   return (
     <div>
+        <StaticQuery 
+          query={`${navigationQuery}`}
+          render={(data) => {
+            console.log(data)
+            return data.prismic.allNavigations.edges[0].node.navigation_links.map((link) => {
+              return <NavLink key={link.link._meta.uid}>
+                <Link to={`/${link.link._meta.uid}`}>
+                  {link.label}
+                </Link>
+              </NavLink>
+            })
+          }} 
+        />
         <Main>{children}</Main>
     </div>
   )
