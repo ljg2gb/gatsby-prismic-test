cbb432967821af45a75de74bab5a5335f4b450bd
Lydia Gregory <ljg2gb@virginia.edu>
2020-09-17 12:29:46 -0400

    adds pprismic plugin w/ options to generate new pages from prismic data

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,19 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: "@prismicio/gatsby-source-prismic-graphql",
+      options: {
+        repositoryName: "gatsby-prismic-test-project",
+        pages: [{
+          type: 'Page',
+          match: '/:uid',
+          path: '/',
+          component: require.resolve('./src/templates/page.js')
+        }]
+
+      }
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
