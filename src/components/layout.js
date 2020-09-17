52c16c61b2f60695929a439a67f8c7de66bfdf51
Lydia Gregory <ljg2gb@virginia.edu>
2020-09-17 15:44:10 -0400

    installs gatsby-plugin-styled-components, styled-components, and babel-plugin-styled-components and adds first styled component in layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
 import "./layout.css"
+import styled from 'styled-components';
+
+const Main = styled.main`
+  max-width: 800px;
+  margin: 0 auto
+`
 
 const Layout = ({ children }) => {
   
 
   return (
     <div>
-        <main>{children}</main>
+        <Main>{children}</Main>
     </div>
   )
 }
